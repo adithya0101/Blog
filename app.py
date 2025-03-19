840a494becb1678ebf7788007988ce02fd09c083
Adithya Veldurthi <134225145+adithya0101@users.noreply.github.com>
2025-03-19 18:55:26 +0000

    Added templates,first working outline of Blogapp

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-app=Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template('templates/index.html')
+    return render_template('index.html')
+def view_blog():
+    pass
+def add_blog():#compose new blog
+    pass
+def home():#home button
+    pass
+def about():#profile page
+    pass
+#LOGIN
+def login():#login page
+    pass
+def logout():#logout page
+    pass
+
+   
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
+
