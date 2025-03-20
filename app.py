b822754932c0e3e1959d1d54d173c1a2c20aa4cc
Adithya Veldurthi <134225145+adithya0101@users.noreply.github.com>
2025-03-20 17:50:11 +0000

    added api routes, integrated databases on supabase

--- app.py
+++ app.py
@@ -2,23 +2,47 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.kfverqmedtlikctuvfob:Blogdbpswrd@1@aws-0-ap-south-1.pooler.supabase.com:6543/postgres'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
-def view_blog():
-    pass
-def add_blog():#compose new blog
-    pass
-def home():#home button
-    pass
-def about():#profile page
-    pass
-#LOGIN
+    
+@app.route('/login')
 def login():#login page
-    pass
+    return render_template('api/login.html')
+
+@app.route('/signup')
+def signup():#signup page
+    return render_template('api/signup.html')
+
+@app.route('/myblog')
+def view_blog():
+    return render_template('api/myblog.html')
+    
+@app.route('/createblog')
+def add_blog():#compose new blog
+    return render_template('api/createblog.html')
+    
+@app.route('/home')
+def home():#home button
+    return render_template('api/index.html')
+    
+@app.route('/profile')
+def about():#profile page
+    return render_template('api/profile.html')
+    
+
+    
+@app.route('/userlogout')
 def logout():#logout page
-    pass
+    return(render_template('index.html'))
 
    
 
